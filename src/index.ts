76bdccf34ae6bf84e23611908b989e130fd2840a
HeinousTugboat <admin@heinousindustries.com>
2017-07-18 17:21:37 -0400

    feat: initial database stuff added

--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,27 @@
 // http://expressjs.com/en/starter/generator.html
 import express = require('express');
 const app = express();
+const initOptions = {
+    // 'postgres://cumulus:nineball@localhost:5432/cumulonimbus'
+}
+const pgp = require('pg-promise')(initOptions);
+const db = pgp('postgres://cumulus:nineball@localhost:60888/cumulonimbus');
 
 app.get('/', function(req, res) {
     res.send('Hello world!');
+    console.log(req);
 })
 
 app.listen(58808, function() {
     console.log('Cumulus listening on port 58808!');
 })
 
-console.log(2+2);
 console.log('Cumulus started...');
 
-console.log('Cumulus.. finished.. *cough*cough*die*');
+db.one('SELECT $1 AS value', 123)
+    .then(function(data:any) {
+        console.log('DATA:', data.value);
+    })
+    .catch(function(error:any) {
+        console.log('ERROR:', error);
+    })
