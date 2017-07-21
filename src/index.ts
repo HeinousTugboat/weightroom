e4c707bbb35df78938f4779a00ced84b4ced068a
HeinousTugboat <admin@heinousindustries.com>
2017-07-20 22:28:54 -0400

    feat: playing with rxjs and node debug

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,15 @@
 // http://mherman.org/blog/2016/11/05/developing-a-restful-api-with-node-and-typescript/#.WWvhKIjyuUk
 // http://expressjs.com/en/starter/generator.html
 import express = require('express');
+const debug = require('debug');
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin//,
+    // output: process.stdout
+});
+const log = debug('cumulus:log');
+const logDB = debug('xcumulus:db');
+const logRx = debug('cumulus:Rx');
 const app = express();
 const initOptions = {
     // 'postgres://cumulus:nineball@localhost:5432/cumulonimbus'
@@ -11,19 +20,67 @@ const db = pgp('postgres://cumulus:nineball@localhost:60888/cumulonimbus');
 
 app.get('/', function(req, res) {
     res.send('Hello world!');
-    console.log(req);
+    log(req);
 })
 
 app.listen(58808, function() {
-    console.log('Cumulus listening on port 58808!');
+    log('Cumulus listening on port 58808!');
 })
 
-console.log('Cumulus started...');
+log('Cumulus started...');
 
 db.one('SELECT * FROM workouts WHERE workout_id = 1', 123)
     .then(function(data:any) {
-        console.log('DATA:', data);
+        logDB('DATA:', data);
     })
     .catch(function(error:any) {
-        console.log('ERROR:', error);
+        logDB('ERROR:', error);
+    })
+
+// console.log('\n\n\n');
+import * as Rx from 'rxjs/Rx';
+
+// logRx(Rx.Observable.of(1, 2, 3));
+
+// rl.on('line', (line: any)=>{
+//     log(`Beep: ${line}`);
+// })
+let subject = new Rx.ReplaySubject();
+// let lines = Rx.Observable.fromEvent(rl, 'line').subscribe((x: string) => subject.next(x));
+let lines = Rx.Observable.fromEvent(rl, 'line').subscribe(subject);
+// let source = lines;
+// let source = Rx.Observable.range(1, 5).scan((acc, x) => acc + x)
+// let source = Rx.Observable.create((observer: Rx.Observer<string>) => {
+//     observer.next(''+3);
+//     // var id = setInterval(() => {
+//         observer.next('beep')
+//     // }, 1000);
+// });
+let source = subject;
+
+let subscription = source.subscribe((x: any) => logRx('sub '+x),
+               (err:any) => log('Error!', err),
+               () => logRx('Source complete..'));
+
+let sub2 = source.subscribe(x => logRx('sub2 ' + x));
+
+subject.next('Test One');
+subject.next('Test Two');
+
+let sub3 = source.subscribe(x => {
+    if (x === 'test') {
+        subject.next('Foofoo');
+    }
+    logRx('subLate ' + x)
+});
+subject.next('Test Three');
+// subject.complete();
+
+let counter = source.scan((num: number) => (++num), 0).subscribe(x=>logRx(x))
+
+let ticker = Rx.Observable.interval(1000)
+    .bufferTime(3000)
+    .subscribe((x)=>{
+    // subject.next('tick!tock?');
+        logRx('tick!tock? ', x);
     })
